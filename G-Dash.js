5f19a36807fb4f5eb9c97818854af5080a876779
Hecto <hillarynimbedam@gmail.com>
2026-05-17 15:14:46 +0000

    G-Dash.js

--- G-Dash.js
+++ G-Dash.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         G-Dash
 // @namespace    https://github.com/hect0o
-// @version      1
+// @version      1.0.0
 // @description  Tablet-style dashboard for GeoFS — live map, flight logbook, multi-pilot support
 // @author       hecto.oooo
 // @match        https://www.geo-fs.com/geofs.php*
